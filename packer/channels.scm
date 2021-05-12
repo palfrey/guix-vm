6fc6f16aa772ec7134b4735f8667695fd4cde10e
Jb DOYON <jb@jiby.tech>
2021-05-12 22:02:48 +0100

    Remove 1.2.0 guix branch from channel
    Now implicitly using master without restriction

--- packer/channels.scm
+++ packer/channels.scm
@@ -1,7 +1,7 @@
 (list (channel
     (name 'guix)
     (url "git://git.savannah.gnu.org/guix.git")
-    (branch "version-1.2.0")
+    ;; (tag "v1.3.0")
     (introduction (make-channel-introduction
    "9edb3f66fd807b096b48283debdcddccfea34bad"     ;2020-05-26
    (openpgp-fingerprint
