4324b0ade242d706607c2f57dbde73fb74ff366a
Jb DOYON <jb@jiby.tech>
2021-05-15 23:56:47 +0100

    Use guix tag v1.3.0 for reproducibility
    Apparently "tag" is not a valid keyword for channels, but commits are.
    Using the commit of v1.3.0 instead of the tag name for reproducibility.

--- packer/channels.scm
+++ packer/channels.scm
@@ -1,7 +1,7 @@
 (list (channel
     (name 'guix)
     (url "git://git.savannah.gnu.org/guix.git")
-    ;; (tag "v1.3.0")
+    (commit "a0178d34f582b50e9bdbb0403943129ae5b560ff"); tag v1.3.0
     (introduction (make-channel-introduction
    "9edb3f66fd807b096b48283debdcddccfea34bad"     ;2020-05-26
    (openpgp-fingerprint
