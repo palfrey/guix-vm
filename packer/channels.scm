5e9a74f4727f512cae64f71c57c17b0942e6295c
Tom Parker-Shemilt <palfrey@tevp.net>
2026-05-31 12:45:59 +0100

    Re-enable codeberg

--- packer/channels.scm
+++ packer/channels.scm
@@ -1,8 +1,6 @@
 (list (channel
     (name 'guix)
-    ; I'd use this, but SH is down https://status.softwareheritage.org/pages/maintenance/578e5eddcdc0cc7951000520/6a0d821d817e4705f08c6fc4
-    ;(url "https://codeberg.org/guix/guix.git")
-    (url "https://github.com/Millak/guix.git")
+    (url "https://codeberg.org/guix/guix.git")
     (commit "v1.4.0")
     (introduction (make-channel-introduction
    "9edb3f66fd807b096b48283debdcddccfea34bad"     ;2020-05-26
